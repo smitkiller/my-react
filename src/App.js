11232ca81579d18404ca4b966693ef4c0a649f87
smitkiller <wsxmit@gmail.com>
2020-08-03 11:25:57 +0700

    update module packages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
   render() {
     return (
@@ -13,6 +14,11 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <p>Please sign-in:</p>
+          
+
+        </div>
       </div>
     );
   }
